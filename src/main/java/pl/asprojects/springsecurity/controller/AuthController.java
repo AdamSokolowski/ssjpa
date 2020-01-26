a1933cae0aa66299f16cf786c9579d7b2174f5a3
AdamSokolowski <a.sokolowski84@gmail.com>
2020-01-26 11:51:01 +0100

    Added links in views

--- src/main/java/pl/asprojects/springsecurity/controller/AuthController.java
+++ src/main/java/pl/asprojects/springsecurity/controller/AuthController.java
@@ -8,17 +8,17 @@ public class AuthController {
 	
 	@GetMapping("/") 
 		public String home() {
-			return ("<h1>Welcome</h1>");
+			return ("<h1>Welcome</h1><br><a href='/user'>User Page</a><br><a href='/admin'>Admin Page</a>");
 		}
 		
 	@GetMapping("/user")
 	public String user() {
-		return ("<h1>Welcome User</h1>");
+		return ("<h1>Welcome User</h1><br><a href='/admin'>Admin Page</a>");
 	}
 
 	@GetMapping("/admin")
 	public String admin() {
-		return ("<h1>Welcome Admin</h1>");
+		return ("<h1>Welcome Admin</h1><br><a href='/user'>User Page</a>");
 	}
 
 }
